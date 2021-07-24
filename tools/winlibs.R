4482a960b0856d9c4f55baaa0663cb2229034004
Jeroen Ooms <jeroenooms@gmail.com>
2021-07-24 12:54:56 +0200

    Windows: update to GDAL 3.2.1 and fix for UCRT support

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -6,9 +6,8 @@ if(getRversion() < "3.3.0") {
 
 # For details see: https://github.com/rwinlib/gdal2
 VERSION <- commandArgs(TRUE)
-if(!file.exists(sprintf("../windows/gdal2-%s/include/gdal/gdal.h", VERSION))){
-  if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/gdal2/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+if(!file.exists(sprintf("../windows/gdal3-%s/include/gdal-%s/gdal.h", VERSION, VERSION))){
+  download.file(sprintf("https://github.com/rwinlib/gdal3/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
